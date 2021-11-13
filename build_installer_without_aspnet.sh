a0fbcdfed1b2eeffd60de5d38c8f8d7b3f57f978
SeC <sec@thinkcode.pl>
2021-11-13 11:11:10 +0100

    Download internal zip during installer build to fail less

--- build_installer_without_aspnet.sh
+++ build_installer_without_aspnet.sh
@@ -3,6 +3,10 @@
 mkdir -p installer/artifacts/obj/redist/Release/downloads/
 mkdir -p aspnetcore/artifacts/packages/Release/Shipping
 
+if [ ! -f installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-6.0.100-rtm.21527.8.zip ]; then
+    wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/6.0.100-rtm.21527.8/dotnet-toolset-internal-6.0.100-rtm.21527.8.zip -O installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-6.0.100-rtm.21527.8.zip
+fi
+
 cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-x64.tar.gz installer/artifacts/obj/redist/Release/downloads/
 cp sdk/artifacts/packages/Release/NonShipping/dotnet-toolset-internal-*.zip installer/artifacts/obj/redist/Release/downloads/
 

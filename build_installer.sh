ad434dc754c115a2f0766a3e4452a501d10b3cf2
Szczepan <sec@thinkcode.pl>
2022-08-14 12:19:57 +0000

    Refactor build installer scripts, removed duplicate runtime patch

--- build_installer.sh
+++ build_installer.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+ASPNET="${1:-true}"
+
 mkdir -p installer/artifacts/obj/redist/Release/downloads/
 cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-x64.tar.gz installer/artifacts/obj/redist/Release/downloads/
 cp aspnetcore/artifacts/installers/Release/aspnetcore-runtime-* installer/artifacts/obj/redist/Release/downloads/
 cp sdk/artifacts/packages/Release/NonShipping/dotnet-toolset-internal-*.zip installer/artifacts/obj/redist/Release/downloads/
 
-installer/build.sh -c Release -ci -pack --runtime-id freebsd-x64 /p:OSName=freebsd /p:OfficialBuildId=`date +%Y%m%d`.99 -v d
+TAG=6.0.400-rtm.22368.21
+TOOLSET=https://dotnetbuilds.blob.core.windows.net/public/Sdk/$TAG/dotnet-toolset-internal-$TAG.zip
+
+if [ ! -f installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-$TAG.zip ]; then
+    wget $TOOLSET -O installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-$TAG.zip
+fi
+
+installer/build.sh -c Release -ci -pack --runtime-id freebsd-x64 /p:OSName=freebsd /p:OfficialBuildId=`date +%Y%m%d`.99 /p:IncludeAspNetCoreRuntime=$ASPNET -v d
--- build_installer_without_aspnet.sh
+++ build_installer_without_aspnet.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
 
-mkdir -p installer/artifacts/obj/redist/Release/downloads/
-mkdir -p aspnetcore/artifacts/packages/Release/Shipping
-
-if [ ! -f installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-6.0.100-rtm.21527.8.zip ]; then
-    wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/6.0.100-rtm.21527.8/dotnet-toolset-internal-6.0.100-rtm.21527.8.zip -O installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-6.0.100-rtm.21527.8.zip
-    wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/6.0.101-servicing.21569.16/dotnet-toolset-internal-6.0.101-servicing.21569.16.zip -O installer/artifacts/obj/redist/Release/downloads/dotnet-toolset-internal-6.0.101-servicing.21569.16.zip
-fi
-
-cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-x64.tar.gz installer/artifacts/obj/redist/Release/downloads/
-cp sdk/artifacts/packages/Release/NonShipping/dotnet-toolset-internal-*.zip installer/artifacts/obj/redist/Release/downloads/
-
-installer/build.sh -c Release -ci -pack --runtime-id freebsd-x64 /p:OSName=freebsd /p:OfficialBuildId=20211229.99 /p:IncludeAspNetCoreRuntime=false
+./build_installer.sh false

8d27912f35e3f3d9bba01050a316ec49f80ad077
Szczepan <sec@thinkcode.pl>
2023-01-06 19:18:00 +0000

    Detect arch in aspnet/installer builds

--- build_aspnetcore.sh
+++ build_aspnetcore.sh
@@ -1,7 +1,16 @@
 #!/bin/csh
+# Note for arm64 build, it will (or can) fail with grpc csharp plugin, as there's no binary, when it will fail, simply do something like
+# cp /usr/local/bin/grpc_csharp_plugin ~/dotnet-core-freebsd-source-build/aspnetcore/.packages/grpc.tools/2.49.0/tools/linux_x64/grpc_csharp_plugin
+# adjust destination path based on error and version of package used
+
+if (`uname -m` == "arm64") then
+    setenv ARCH arm64
+else
+    setenv ARCH x64
+endif
 
 mkdir -p aspnetcore/artifacts/obj/Microsoft.AspNetCore.App.Runtime
-cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-x64.tar.gz aspnetcore/artifacts/obj/Microsoft.AspNetCore.App.Runtime
+cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-$ARCH.tar.gz aspnetcore/artifacts/obj/Microsoft.AspNetCore.App.Runtime
 
 setenv PROTOBUF_PROTOC /usr/local/bin/protoc
 setenv PROTOBUF_TOOLS_CPU x64
@@ -12,4 +21,4 @@ service linux onestart
 
 setenv TAG `cat aspnetcore.tag`
 
-aspnetcore/eng/build.sh -ci --os-name freebsd -pack -c Release --all /p:TreatWarningsAsErrors=false /p:OfficialBuildId=`./common.sh $TAG`
+aspnetcore/eng/build.sh -ci --os-name freebsd -pack -c Release --all /p:TreatWarningsAsErrors=false /p:OfficialBuildId=`./common.sh $TAG` --arch $ARCH
--- build_installer.sh
+++ build_installer.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/csh
+
+if (`uname -m` == "arm64") then
+    setenv ARCH arm64
+else
+    setenv ARCH x64
+endif
 
 mkdir -p installer/artifacts/obj/redist/Release/downloads/
-cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-x64.tar.gz installer/artifacts/obj/redist/Release/downloads/
+cp runtime/artifacts/packages/Release/Shipping/dotnet-runtime-*-freebsd-$ARCH.tar.gz installer/artifacts/obj/redist/Release/downloads/
 cp aspnetcore/artifacts/installers/Release/aspnetcore-runtime-* installer/artifacts/obj/redist/Release/downloads/
 cp sdk/artifacts/packages/Release/NonShipping/dotnet-toolset-internal-*.zip installer/artifacts/obj/redist/Release/downloads/
 
-TAG=`cat installer.tag`
-
-installer/build.sh -c Release -ci -pack --runtime-id freebsd-x64 /p:OSName=freebsd /p:OfficialBuildId=`./common.sh $TAG` /p:IncludeAspNetCoreRuntime=true
+setenv TAG `cat installer.tag`
+installer/build.sh -c Release -ci -pack --runtime-id freebsd-$ARCH /p:OSName=freebsd /p:OfficialBuildId=`./common.sh $TAG` /p:IncludeAspNetCoreRuntime=true

61062bea3a8bea41b604ca8a7a804f38920f4ac6
Szczepan Ćwikliński <sec@thinkcode.pl>
2023-04-17 12:38:00 +0200

    use llvm14 in install script, don't install linux by default

--- install_tools.sh
+++ install_tools.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-pkg install autoconf libtool automake cmake llvm10 krb5 terminfo-db libunwind icu libinotify python3 pkgconf binutils npm-node14 node14 openjdk11 bash git wget protobuf linux_base-c7 grpc
+pkg install autoconf libtool automake cmake llvm14 krb5 terminfo-db libunwind icu libinotify python3 pkgconf binutils npm-node14 node14 openjdk11 bash git wget protobuf grpc
 
-service linux onestart
+#linux_base-c7 grpc
+#service linux onestart

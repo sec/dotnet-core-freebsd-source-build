c89492fe6d6b2f254bce80319785c1c52e76cfb9
Szczepan Ćwikliński <sec@thinkcode.pl>
2023-08-18 15:21:44 +0200

    update tools pkg install

--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pkg install autoconf libtool automake cmake llvm14 krb5 terminfo-db libunwind icu libinotify python3 pkgconf binutils npm-node14 node14 openjdk11 bash git wget protobuf grpc
+pkg install autoconf libtool automake cmake llvm14 krb5 terminfo-db libunwind icu libinotify python3 pkgconf binutils npm-node18 node18 openjdk11 bash git wget protobuf grpc openssl
 
 #linux_base-c7 grpc
 #service linux onestart

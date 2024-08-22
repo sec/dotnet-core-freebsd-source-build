bdb7dc73353947a7dab5becf00ff2339e2507eeb
Szczepan Ćwikliński <sec@thinkcode.pl>
2024-08-22 18:44:41 +0200

    fix for apphost in sdk

--- bsd_dotnet_install.sh
+++ bsd_dotnet_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/csh
 
 # Fake .NET Core Installer for FreeBsd ;)
 # $1 - SDK BIN for FreeBSD
@@ -9,4 +9,13 @@ echo Extracting .NET Core SDK into $2...
 mkdir -p $2/.dotnet
 tar zxf $1 --directory $2/.dotnet
 elfctl -e +noaslr $2/.dotnet/dotnet
+
+if (`uname -m` == "arm64") then
+    setenv ARCH arm64
+else
+    setenv ARCH x64
+endif
+
+sed -i '' -e 's/linux-x64/freebsd-$ARCH/' $2/.dotnet/sdk/9.0.100-preview.7.24407.12/Microsoft.NETCoreSdk.BundledVersions.props
+
 echo 'exit 0' > $2/.dotnet/dotnet-install.sh
